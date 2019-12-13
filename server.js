2a25f85f4df8351c977d3fbe4d2acf57f50080c0
Addison Hill <adddison.hill@gmail.com>
2019-12-13 13:01:38 -0600

    done with everything except getting project resource

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const ProjectRouter = require("./projects/projects-router");
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use("/api/projects", ProjectRouter);
 
 server.get("/", (req, res) => {
   res.send("<h3>This Sprint is FUN</h3>");
